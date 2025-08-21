bef3da30676cc82dd9646f4695d99b6adee4d9e3
shadow1912150 <luislaceramejia@gmail.com>
2025-08-21 09:22:21 -0500

    Segunda parte

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+public class Animales
+{
+    public int id_Raza;
+    public string? Nombre;
+    public decimal tamaño;
+    public decimal peso;
+    public bool Vive;
+    public DateTime FechaN;
+    public Carnivoros? carnivoro;
+
+}
+public class Carnivoros
+{
+    public int id_Raza;
+    public string? Nombre;
+    public decimal tamaño;
+    public decimal peso;
+       
+}
